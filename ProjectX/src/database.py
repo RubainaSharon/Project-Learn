d1970ee41bf1a61f5203dbd8a43698162b108b3e
Rubaina <86163677+RubainaSharon@users.noreply.github.com>
2025-04-27 01:05:46 +0530

    Update database.py

--- ProjectX/src/database.py
+++ ProjectX/src/database.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://neondb_owner:npg_4oGaOb5VDTWc@ep-polished-bread-a194c7dg-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require
-"
+DATABASE_URL = "postgresql://neondb_owner:npg_4oGaOb5VDTWc@ep-polished-bread-a194c7dg-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
