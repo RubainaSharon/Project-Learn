b230e8ce3040f5399f29a6469188055e22c09c62
Rubaina <86163677+RubainaSharon@users.noreply.github.com>
2025-05-04 23:25:59 +0530

    Update Quiz.js

--- quiz-frontend/src/Quiz.js
+++ quiz-frontend/src/Quiz.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
 import Navbar from "./navbar";
+import "./Quiz.css";
 
 const Quiz = ({ username }) => {
   const { skill } = useParams();
